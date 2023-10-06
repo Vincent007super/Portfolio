2ba3e69673efbc6674e72ee405ea3826c60836be
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2023-10-06 12:27:06 +0200

    Dark/theme made with css and js. Contrast still needs to be added

---
+++ Pages/js/theme.js
@@ -0,0 +1,13 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const body = document.body;
+const back = document.querySelector('.back');
+
+toggleTheme.addEventListener('change', function() {
+    if (this.checked) {
+        body.style.setProperty('--text', 'var(--dark-text)');
+        body.style.setProperty('--bg', 'var(--dark-bg)');
+    } else {
+        body.style.setProperty('--text', 'var(--light-text)');
+        body.style.setProperty('--bg', 'var(--light-bg)');
+    }
+});
