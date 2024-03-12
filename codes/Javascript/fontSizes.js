8af6f5cb04a0a29eab988a708a8ad8f58800e410
Vincent007super <vrhoogenraat11@gmail.com>
2024-03-12 22:48:27 +0100

    Updated Font size
    Changed js file to lower the amount fonts get upscaled to

--- codes/Javascript/fontSizes.js
+++ codes/Javascript/fontSizes.js
@@ -3,6 +3,6 @@ const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 headings.forEach(heading => {
     const computedFontSize = window.getComputedStyle(heading).getPropertyValue('font-size');
     const currentFontSize = parseFloat(computedFontSize);
-    const newFontSize = currentFontSize * 2;
+    const newFontSize = currentFontSize * 1.5;
     heading.style.fontSize = `${newFontSize}px`;
 });
